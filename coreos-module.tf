8b3b110a3df289192a01dc3358f52c5cd0df5edd
bri <284789+b-@users.noreply.github.com>
2025-07-10 13:55:46 -0400

    test change

--- coreos-module.tf
+++ coreos-module.tf
@@ -7,7 +7,16 @@ module "coreos-module-vm" {
   pve_disk_datastore_id = "zssd"
   pve_iso_datastore_id  = "zssd-files"
   vm_agent_enabled      = false
-
+}
+module "coreos-module-vm2" {
+  source   = "./modules/coreos-vm"
+  password = onepassword_item.coreos_module_password.password
+  username = onepassword_item.coreos_module_password.username
+  #vm_vga_type = "serial0"
+  vm_authorized_keys    = [data.onepassword_item.proxmox_ssh.note_value]
+  pve_disk_datastore_id = "zssd"
+  pve_iso_datastore_id  = "zssd-files"
+  vm_agent_enabled      = false
 }
 resource "onepassword_item" "coreos_module_password" {
   vault    = local.perchnet_vault
