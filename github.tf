5abe4d02063dea5779dc235c01fca7d9211df76a
bri <284789+b-@users.noreply.github.com>
2025-07-11 02:05:12 -0400

    import/github repository (#17)
    * import and manage this github repository
    
    * pre-commit: fork, automatically fix fmt errors
    
    * pre-commit: disable validate, ci does that for us
    
    * gh repo: merge commit message

--- github.tf
+++ github.tf
@@ -23,39 +23,39 @@ locals {
 
 }
 # This repository
-# resource "github_repository" "p_repository" {
-#   name = "p"
-# 
-#   allow_auto_merge            = false
-#   allow_merge_commit          = true
-#   allow_rebase_merge          = true
-#   allow_squash_merge          = true
-#   allow_update_branch         = false
-#   archived                    = false
-#   delete_branch_on_merge      = false
-#   description                 = null
-#   has_discussions             = false
-#   has_downloads               = true
-#   has_issues                  = true
-#   has_projects                = true
-#   has_wiki                    = false
-#   homepage_url                = null
-#   is_template                 = false
-#   merge_commit_message        = "PR_TITLE"
-#   merge_commit_title          = "MERGE_MESSAGE"
-#   squash_merge_commit_message = "COMMIT_MESSAGES"
-#   squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
-#   topics                      = []
-#   visibility                  = "public"
-#   vulnerability_alerts        = true
-#   web_commit_signoff_required = false
-# 
-#    security_and_analysis {
-#      secret_scanning {
-#        status = "disabled"
-#      }
-#      secret_scanning_push_protection {
-#        status = "disabled"
-#      }
-#    }
-# }
+resource "github_repository" "p_repository" {
+  name = "p"
+
+  allow_auto_merge            = true
+  allow_merge_commit          = true
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  allow_update_branch         = true
+  archived                    = false
+  delete_branch_on_merge      = false
+  description                 = null
+  has_discussions             = false
+  has_downloads               = true
+  has_issues                  = true
+  has_projects                = true
+  has_wiki                    = false
+  homepage_url                = null
+  is_template                 = false
+  merge_commit_message        = "PR_BODY"
+  merge_commit_title          = "PR_TITLE"
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+  topics                      = []
+  visibility                  = "public"
+  vulnerability_alerts        = true
+  web_commit_signoff_required = false
+
+  security_and_analysis {
+    secret_scanning {
+      status = "disabled"
+    }
+    secret_scanning_push_protection {
+      status = "disabled"
+    }
+  }
+}
