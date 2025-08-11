d47c5a66d368d53a1423f2fdf1182946b8bfca6c
bri <284789+b-@users.noreply.github.com>
2025-08-10 22:49:15 -0400

    Tailscale/ubuntu test vm (#43)
    * add custom cloud-init and stuff
    
    * remove coreos vm for now
    
    * no-op change to kick the actions
    
    * lists cannot contain null values
    
    * plan again
    
    * Update guests.tf
    
    * Update guests.tf
    
    * Update guests.tf
    
    * turn off guest agent on templates
    
    workaround because it hangs refreshing templates when guest agent is
    enabled on them. possibly related to https://github.com/bpg/terraform-provider-proxmox/issues/2091
    
    * update modules

--- guests.tf
+++ guests.tf
@@ -2,37 +2,12 @@
 #  source            = "./modules/ubuntu-vm"
 #  import_from_image = module.proxmox_images.images["ubuntu_jammy"].id
 #}
-module "ubuntu22" {
-  source = "github.com/b-/terraform-bpg-proxmox//modules/vm-template?ref=ab72f1a"
-
-
-  disk_storage = "zssd"
-  #scsihw = "virtio-scsi-single"
-  efi_disk_storage = "zssd"
-  ci_datastore_id  = "zssd"
-  #disks = [ {disk_storage = "zssd"} ]
-
-  node = local.pve_node # Required
-
-  # Image Variables
-  image_url                = "https://cloud-images.ubuntu.com/releases/22.04/release-20240207/ubuntu-22.04-server-cloudimg-amd64.img" # Required
-  image_checksum           = "7eb9f1480956af75359130cd41ba24419d6fd88d3af990ea9abe97c2f9459fda"                                       # Required
-  image_checksum_algorithm = "sha256"                                                                                                 # Optional
-  image_overwrite          = false                                                                                                    # Optional
-
-  # VM Template Variables
-  vm_id       = 8022                                             # Required
-  vm_name     = "ubuntu22"                                       # Optional
-  description = "Terraform generated template on ${timestamp()}" # Optional
-  tags        = ["terraform", "template", "ubuntu"]              # Optional
-  #ci_vendor_data = "local:snippets/vendor-data.yaml"                # Optional
-}
 locals {
   ubuntu_vm_name = "vm-ubuntu22-minimal"                          # optional
   ci_ssh_keys    = [data.onepassword_item.proxmox_ssh.public_key] # optional, add SSH key to "default" user
 }
 module "vm_minimal_config" {
-  source = "github.com/b-/terraform-bpg-proxmox//modules/vm-clone?ref=ab72f1a"
+  source = "github.com/b-/terraform-bpg-proxmox//modules/vm-clone?ref=97989d3"
 
   scsihw = "virtio-scsi-single"
   #efi_disk_storage = "zssd"
@@ -41,10 +16,10 @@ module "vm_minimal_config" {
   qemu_guest_agent    = true
   ci_snippets_storage = "snippets"
 
-  node                  = local.pve_node
-  vm_id                 = 10000 # required
+  node = local.pve_node
+  #vm_id                 = 10000 # required
   vm_name               = local.ubuntu_vm_name
-  template_id           = 8022                                           # required
+  template_id           = module.ubuntu22.id
   ci_ssh_keys           = [data.onepassword_item.proxmox_ssh.public_key] # optional, add SSH key to "default" user
   ci_user_data_contents = <<-EOF
     #cloud-config
@@ -60,7 +35,7 @@ module "vm_minimal_config" {
 }
 
 resource "tailscale_tailnet_key" "tailscale_key" {
-  reusable      = false
+  reusable      = true
   ephemeral     = false               # Keep node when offline
   preauthorized = true                # Auto-authorize
   expiry        = 600                 #local.rotation_seconds
@@ -70,32 +45,6 @@ resource "tailscale_tailnet_key" "tailscale_key" {
   #  }
 }
 
-module "debian13" {
-  source = "github.com/b-/terraform-bpg-proxmox//modules/vm-template?ref=ab72f1a"
-
-
-  #scsihw = "virtio-scsi-single"
-  efi_disk_storage = "zssd"
-  ci_datastore_id  = "zssd"
-  #disks = [ {disk_storage = "zssd"} ]
-  disk_storage = "zssd"
-
-  node = local.pve_node
-
-  # Image Variables
-  image_filename           = "debian-13-generic-amd64.img" # Convert *.qcow2 image to *.img
-  image_url                = "https://cloud.debian.org/images/cloud/trixie/20250806-2196/debian-13-generic-amd64-20250806-2196.qcow2"
-  image_checksum           = "a7dfe434afc40afb0a791c777f3edba6b1a5c4b7315a61073fe5e34752d3bc5fd44ff67ef054eb4263687a97f7ce4896bf5bad5f216ef8b9d4a84541759e743d"
-  image_checksum_algorithm = "sha512"
-  image_overwrite          = false
-
-  # VM Template Variables
-  vm_id       = 9013
-  vm_name     = "debian13"
-  description = "Terraform generated template on ${timestamp()}"
-  tags        = ["terraform", "template", "debian"]
-  #ci_vendor_data = "local:snippets/vendor-data.yaml"
-}
 # locals {
 #   cloud_init_datastore_id = "zssd-files"
 # }
