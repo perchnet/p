d296d7c689c6a22974ee5e38d3823d3da356a79a
bri <284789+b-@users.noreply.github.com>
2025-08-14 23:30:32 -0400

    fix timestamp replacement logic

--- guests.tf
+++ guests.tf
@@ -7,7 +7,7 @@ locals {
   ci_ssh_keys    = [data.onepassword_item.proxmox_ssh.public_key] # optional, add SSH key to "default" user
 }
 module "vm_minimal_config" {
-  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=5c3ca92"
+  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=b8088a2"
   scsihw = "virtio-scsi-single"
   cloudinit = {
     datastore_id     = "zssd-files"
