39de3033c8fa764676547c026adad68f315d68dc
bri <284789+b-@users.noreply.github.com>
2025-08-14 23:45:43 -0400

    start vm by default if not a template

--- templates.tf
+++ templates.tf
@@ -1,5 +1,5 @@
 module "debian13" {
-  source   = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=b8088a2"
+  source   = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=b77647c"
   template = true
 
   efi = {
@@ -25,7 +25,7 @@ module "debian13" {
   tags             = ["terraform", "template", "debian"]
 }
 module "ubuntu22" {
-  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=b8088a2"
+  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=b77647c"
   #source = "/var/home/bri/dev/terraform-proxmox-modules/modules/vm"
 
   template = true
