f0bb9dea1855de38cc193590e66fa935fe4d6d2a
bri <284789+b-@users.noreply.github.com>
2025-08-14 21:14:20 -0400

    weird bools

--- templates.tf
+++ templates.tf
@@ -1,5 +1,5 @@
 module "debian13" {
-  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=845f30d"
+  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=7b70db4"
   #source = "/home/bri/dev/terraform-proxmox-modules/modules/vm"
 
   efi = {
@@ -9,12 +9,12 @@ module "debian13" {
   disks = [{
     storage = "zssd"
     download = {
-      storage            = "zssd-files"
-      filename           = "debian-13-generic-amd64.img" # Convert *.qcow2 image to *.img
-      url                = "https://cloud.debian.org/images/cloud/trixie/20250806-2196/debian-13-generic-amd64-20250806-2196.qcow2"
-      checksum           = "a7dfe434afc40afb0a791c777f3edba6b1a5c4b7315a61073fe5e34752d3bc5fd44ff67ef054eb4263687a97f7ce4896bf5bad5f216ef8b9d4a84541759e743d"
-      checksum_algorithm = "sha512"
-      overwrite          = false
+      storage   = "zssd-files"
+      filename  = "debian-13-generic-amd64.img" # Convert *.qcow2 image to *.img
+      url       = "https://cloud.debian.org/images/cloud/trixie/20250806-2196/debian-13-generic-amd64-20250806-2196.qcow2"
+      checksum  = "a7dfe434afc40afb0a791c777f3edba6b1a5c4b7315a61073fe5e34752d3bc5fd44ff67ef054eb4263687a97f7ce4896bf5bad5f216ef8b9d4a84541759e743d"
+      algorithm = "sha512"
+      overwrite = false
     }
   }]
 
@@ -26,7 +26,7 @@ module "debian13" {
   #ci_vendor_data = "local:snippets/vendor-data.yaml"
 }
 module "ubuntu22" {
-  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=845f30d"
+  source = "github.com/b-/terraform-bpg-proxmox//modules/vm?ref=7b70db4"
   #source = "/var/home/bri/dev/terraform-proxmox-modules/modules/vm"
 
   efi  = {}
@@ -36,13 +36,13 @@ module "ubuntu22" {
     {
       storage = "zssd"
       download = {
-        storage            = "zssd-files"
-        url                = "https://cloud-images.ubuntu.com/releases/22.04/release-20240207/ubuntu-22.04-server-cloudimg-amd64.img" # Required
-        checksum           = "7eb9f1480956af75359130cd41ba24419d6fd88d3af990ea9abe97c2f9459fda"                                       # Required
-        checksum_algorithm = "sha256"                                                                                                 # Optional
-        overwrite          = false                                                                                                    # Optional
-        size               = 20
+        storage   = "zssd-files"
+        url       = "https://cloud-images.ubuntu.com/releases/22.04/release-20240207/ubuntu-22.04-server-cloudimg-amd64.img" # Required
+        checksum  = "7eb9f1480956af75359130cd41ba24419d6fd88d3af990ea9abe97c2f9459fda"                                       # Required
+        algorithm = "sha256"                                                                                                 # Optional
+        overwrite = false                                                                                                    # Optional
       }
+      size = 20
     }
   ]
   # Image Variables
