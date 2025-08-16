561e4953436b0eaff506f6217a9b79889939eec0
bri <284789+b-@users.noreply.github.com>
2025-08-16 15:06:15 -0400

    update image extensions to more accurately reflect content for stricter validation with content_type=imports

--- templates.tf
+++ templates.tf
@@ -9,8 +9,8 @@ module "debian13" {
   disks = [{
     storage = "zssd"
     download = {
-      storage   = "zssd-files"
-      filename  = "debian-13-generic-amd64.img" # Convert *.qcow2 image to *.img
+      storage = "zssd-files"
+      #filename  = "debian-13-generic-amd64.img" # Convert *.qcow2 image to *.img
       url       = "https://cloud.debian.org/images/cloud/trixie/20250806-2196/debian-13-generic-amd64-20250806-2196.qcow2"
       checksum  = "a7dfe434afc40afb0a791c777f3edba6b1a5c4b7315a61073fe5e34752d3bc5fd44ff67ef054eb4263687a97f7ce4896bf5bad5f216ef8b9d4a84541759e743d"
       algorithm = "sha512"
@@ -37,10 +37,11 @@ module "ubuntu22" {
       storage = "zssd"
       download = {
         storage   = "zssd-files"
-        url       = "https://cloud-images.ubuntu.com/releases/22.04/release-20240207/ubuntu-22.04-server-cloudimg-amd64.img" # Required
-        checksum  = "7eb9f1480956af75359130cd41ba24419d6fd88d3af990ea9abe97c2f9459fda"                                       # Required
-        algorithm = "sha256"                                                                                                 # Optional
-        overwrite = false                                                                                                    # Optional
+        url       = "https://cloud-images.ubuntu.com/releases/22.04/release-20240207/ubuntu-22.04-server-cloudimg-amd64.img"   # Required
+        filename  = "https://cloud-images.ubuntu.com/releases/22.04/release-20240207/ubuntu-22.04-server-cloudimg-amd64.qcow2" # Required
+        checksum  = "7eb9f1480956af75359130cd41ba24419d6fd88d3af990ea9abe97c2f9459fda"                                         # Required
+        algorithm = "sha256"                                                                                                   # Optional
+        overwrite = false                                                                                                      # Optional
       }
       #size = 20
     }
